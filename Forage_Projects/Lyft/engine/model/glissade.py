ea9a4c7254dda3742c2f5cf01b64caa3fe8a4dc0
Michael Johnson <michael_johnson144@yahoo.com>
2022-04-13 23:04:30 -0400

    Second commit

--- Forage_Projects/Lyft/engine/model/glissade.py
+++ Forage_Projects/Lyft/engine/model/glissade.py
@@ -6,7 +6,7 @@ from engine.willoughby_engine import WilloughbyEngine
 class Glissade(WilloughbyEngine):
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+        if service_threshold_date < datetime.today().date() or WilloughbyEngine.engine_should_be_serviced(self):
             return True
         else:
             return False
